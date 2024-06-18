363bd7662db55e63d4732a837e8d6d058725a84e
aidil-sekandar <aidiliskandar63@gmail.com>
2024-06-19 02:35:22 +0800

    🚧 Finalize design for home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,14 @@ export default {
 				bodyColor: "#FAFAFA",
 				textPrimary: "#242425",
 				textSecondary: "#616162",
+				// pastel color pallete
+				redPastel: "#f09ea7",
+				orangePastel: "#f6ca94",
+				yellowPastel: "#fafabe",
+				greenPastel: "#d6ffd2",
+				bluePastel: "#c7caff",
+				purplePastel: "#C1BEFA",
+				pinkPastel: "#ffdafd"
 			}
 		},
 	},
